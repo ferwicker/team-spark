0753ddcd6fd93fdbacf3ae9fb8eba18dddc0b356
Fer Wicker <fer.wicker@gmail.com>
2021-04-21 10:06:28 +1000

    nav links change

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,17 +7,17 @@ function Navbar(){
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
             <div className='container'>
-                    <Link to="/">
+                    <Link to="/team-spark">
                         <img className="navbar-brand" src={logo} alt='Teamspark logo'></img>
                     </Link>
                     <div className='d-flex'>
-                    <Link to="/" className='nav-link'>
+                    <Link to="/team-spark" className='nav-link'>
                         Home
                     </Link>
-                    <Link to="/about" className='nav-link'>
+                    <Link to="/team-spark/about" className='nav-link'>
                         About
                     </Link>
-                    <Link to="/help" className='nav-link'>
+                    <Link to="/team-spark/help" className='nav-link'>
                         Help
                     </Link>
                 </div>
